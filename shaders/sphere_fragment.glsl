2553b174fbd10870bdb696be0301cedafeeb0f40
ju__ni <juliandk1357@gmail.com>
2026-01-17 19:59:25 +0100

    sphere shaders

---
+++ shaders/sphere_fragment.glsl
@@ -0,0 +1,8 @@
+#version 450 core
+
+out vec4 fragmentColor;
+
+void main()
+{
+    fragmentColor = vec4(0.7, 0.7, 0.7, 1.0);
+}
---
+++ shaders/sphere_vertex.glsl
@@ -0,0 +1,15 @@
+#version 450 core
+
+layout(location = 0) in vec3 aPos;
+layout(std430, binding = 0) buffer TransformBuffer {
+    mat4 transforms[];
+};
+
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    mat4 modelMatrix = transforms[gl_InstanceID];
+    gl_Position = projection * view * modelMatrix * vec4(aPos, 1.0);
+}
